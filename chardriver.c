bd2767258068f375ead0019879d88c4c520004ae
Karthic Rao <kartronics85@gmail.com>
2013-02-01 06:54:17 +0530

    second commit with simple driver

---
+++ chardriver.c
@@ -0,0 +1,33 @@
+#include <linux/module.h>
+#include <linux/version.h>
+#include <linux/kernel.h>
+#include <linux/types.h>
+#include <linux/types.h>
+#include <linux/kdev_t.h>
+#include <linux/fs.h>
+
+static dev_t first; //global varible for the first device number 
+
+static int __init my_init(void) /*constructor*/
+{
+	int ret;
+	
+	printk(KERN_INFO "hi!!!My driver");
+	if(( ret = alloc_chrdev_region(&region,0,3,"RAO's Driver")) < 0)
+	{
+		return ret;
+	}
+	printk(KERN_INFO "<Major , Minor > <%d , %d >",	MAJOR(first),MINOR(first));
+	return 0;
+}
+static void __exit my_exit(void)
+{
+	unregister_chrdev_region(first,3);
+	printk(KERN_INFO "driver is being removed");
+}
+module_init(my_init);
+module_exit(my_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("KARTHIC RAO");
+MODULE_DESCRIPTION("SIMPLE SRIVER FOR NODE COMMUNICATION PROJECT");
