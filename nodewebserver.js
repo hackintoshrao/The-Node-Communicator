8fb0d158cce7b16bb07e528c723ae66020824e3a
Karthic Rao <kartronics85@gmail.com>
2013-02-13 10:23:55 +0530

    webserver on the go

--- nodewebserver.js
+++ nodewebserver.js
@@ -5,6 +5,24 @@ var http=require('http'),
 http.createServer(function(req,res) {
 	 pathname = search_path + req.url;
 	console.log(pathname);
-
+	path.exists(pathname,function(exits) {
+		if(!exists) {
+			res.writeHead(404);
+			res.write('Invalid Request , PATH doesnt exist 404 error');
+			res.end();
+		else{
+			res.setHeader('Content-Type','text/html');
+			res.writeHead(200);
+			
+			//create a read dtream and direct it to the HTTP response
+			var file = fs.createReadStream(pathname);
+			file.on("open",function() { 
+				file.pipe(res);
+			});
+			file.on("error",function(err) {
+				console.log(err);
+			});
+		}
+	});
 }).listen(8124);
 console.log('Server Running at 8124');
