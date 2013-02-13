c5e01f44b9d4cbc072528c5b3273260489ee6958
Karthic Rao <kartronics85@gmail.com>
2013-02-13 09:25:02 +0530

    Node webserver stage 1

--- nodewebserver.js
+++ nodewebserver.js
@@ -2,4 +2,9 @@ var http=require('http'),
 	path = require('path'),
 	fs = require('fs'),
 	search_path='/home/karthic/mydata/codes/node_support';
+http.createServer(function(req,res) {
+	var pathname = search_path + req.url;
+	consoe.log(pathname);
 
+}).listen(8124);
+console.log('Server Running at 8124');
