fe168b68698de8c71ea6ed6c96c3817efe3ce26f
Karthic Rao <kartronics85@gmail.com>
2013-02-17 07:02:37 +0530

    Webserver on modifying -2

--- nodewebserver.js
+++ nodewebserver.js
@@ -2,7 +2,7 @@ var http=require('http'),
 	url = require('url'),
 	mime = require('mime'),
 	fs = require('fs'),
-	search_path='/home/karthic/mydata/codes/node_support';
+	search_path='/home/karthic/mydata/codes';
 http.createServer(function(req,res) {
 	 pathname = search_path + req.url;
 	 console.log(pathname);
@@ -14,3 +14,29 @@ http.createServer(function(req,res) {
 		res.end()
 		}
 	else if(stats.isFile()) {
+		//content-type
+		var type = mime.lookup(pathname);
+		console.log(type);
+		res.setHeader('Content-Type',type);
+
+		// 200 status - found , no errors 
+		res.statusCode = 200;
+	
+		//create a readable stream and pipe to the http response 
+		var file = fs.createReadStream(pathname);
+		file.on("open",function() { 
+			file.pipe(res);
+		});
+		file.on("error",function(err) {
+			cosole.log(err);
+		});
+	     }
+	else {
+		res.writeHead(403);
+		res.write('Cannot Access Directory');
+		res.end();
+		}
+	});
+}).listen(8124);
+console.log('Server running at 8124');
+		
