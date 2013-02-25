436ffb2e6c117a030eb2f58f113a9637a654851d
Karthic Rao <kartronics85@gmail.com>
2013-02-25 07:53:18 +0530

    reimplementation of web server

--- nodewebserver.js
+++ nodewebserver.js
@@ -1,57 +1,21 @@
-var http=require('http'),
-	url = require('url'),
-	mime = require('mime'),//npm install mime
-	fs = require('fs'),
-	search_path='/home/karthic/mydata/codes';
-http.createServer(function(req,res) {
-	 path = "/home/karthic/codes/driver/git1/linkpage.html";
-	 driver_path = "/dev/mychar0";
-	 console.log(pathname);
+var http = require('http');
+var fs = require('fs').createWriteStream('file1');;
 
-	fs.stat(path , function(err,stats){
-	  if(err) { 
-		res.writeHead(404);
-		res.write('Bad Request , Request cannot be accepted');
-		res.end()
-		}
-	 else if(stats.isDirectory()) {
-		res.writeHead(404);
-		res.write('Cannot access Directory');
-		res.end();
-	}
-	
-	else {
-		fs.stat(driver_path , function(err,device_Stat) {
-			console.log(device_stat);
-		
-		console.log(device_stat);		
-			
-		
-		
-		
-	 	
-	
-	
-		//create a readable stream and pipe to the http response 
-		var file = fs.createWriteStream(pathname);
-		file.on("open",function() { 
-			file.pipe(res);
-		});
-		file.on("error",function(err) {
-			cosole.log(err);
-		});
-	     }
-	else if(!path.exits('/dev/char0')){
-		res.writeHead(403);
-		res.write('Driver not yet loaded , error in the server ');
-		res.end();
-		}
-	else { 
-		res.writeHead(404);
-		res.write('Directory cannot be accessed');
-		res.end();
-		}	
-	});
-}).listen(8124);
-console.log('Server running at 8124');
-		
+http.createServer(function(req, res) {
+  // This opens up the writeable stream to `output`
+  
+
+  // This pipes the POST data to the file
+  req.pipe(fs,{ end:false });
+
+  // After all the data is saved, respond with a simple html form so they can post more data
+  req.on('end', function () {
+    res.writeHead(200, {"content-type":"text/html"});
+    res.end('<form method="POST"><input name="test" /><input type="submit"></form>');
+  });
+
+  // This is here incase any errors occur
+  fs.on('error', function (err) {
+    console.log(err);
+  });
+}).listen(8080);
